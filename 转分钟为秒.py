1f3513d5e45d14f388935a07f7b6ef398564f16b
LuckySHU2003 <Luckyshu2003@gmail.com>
2024-09-11 13:53:01 +1000

    Create 转分钟为秒.py

---
+++ 转分钟为秒.py
@@ -0,0 +1,10 @@
+# 要求：编写一个程序将分钟转换为秒。Hint:一分钟60秒
+
+def convert_to_seconds(minutes):
+  return input_minutes*60
+
+# 输入分钟
+input_minutes = int(input())
+
+# 调用函数 
+print(convert_to_seconds(input_minutes))
