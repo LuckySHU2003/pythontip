58d901a709c28b91bbb103209725f1d2ee3d5536
LuckySHU2003 <Luckyshu2003@gmail.com>
2024-09-11 13:55:56 +1000

    Create 2.字符串转为整数.py

---
+++ 字符串转为整数.py
@@ -0,0 +1,25 @@
+#初始化
+def convert_to_int(str_number):
+    # 在此处编写你的代码
+
+# 获取字符串输入
+input_string = input()
+
+result = convert_to_int(input_string)
+
+# 打印结果的类型
+print(type(result))
+
+#######################################
+
+#答案⬇️
+def convert_to_int(str_number):
+  return int(input_string)
+
+# 获取字符串输入
+input_string = input()
+
+result = convert_to_int(input_string)
+
+# 打印结果的类型
+print(type(result))
